11181a0ba60236d92d91c9b3d103947040d2efa5
Oennn <93218927+Oennn@users.noreply.github.com>
2026-03-10 23:28:41 +0100

    sdv

---
+++ php/phpS3/config.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+define('DB_SERVER', 'mysql.info.unicaen.fr');
+define('DB_PORT', '3306');
+define('DB_NAME', 'chabaud221');
+define('DB_USER', 'chabaud221');
+define('DB_PASS', 'Franca74..');
+define('DB_DSN',
+    "mysql:host=" . DB_SERVER .
+    ";port=" . DB_PORT .
+    ";dbname=" . DB_NAME .
+    ";charset=utf8mb4"
+);
--- php/phpS3/tp8/personnes.php
+++ php/phpS3/tp8/personnes.php
@@ -1,12 +1,21 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=tp8', 'root', '');
+require_once('../config.php');
 
-$stmt = $pdo->query('SELECT * FROM Personne');
-$personnes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($personnes as $personne) {
-    echo $personne['nom'] . ' ' . $personne['prenom'] . '<br>';
+function connecter(): ?PDO
+{
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    try {
+        $pdo = new PDO(DB_DSN, DB_USER, DB_PASS, $options);
+        echo "<p><strong>Connexion établie</strong></p>";
+        return $pdo;
+    } catch (PDOException $e) {
+        error_log("Connexion BD : " . $e->getMessage());
+        echo "<p><strong>Erreur de connexion : </strong>" . $e->getMessage() . "</p>";
+        return null;
+    }
 }
-
-
-?>
+$pdo = connecter();
